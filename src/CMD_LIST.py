765f676671f52d6e403611c2ad49bb3b64e59d79
Jacob O'Brien <jacobrien64@gmail.com>
2022-09-24 23:24:53 -0700

    added git support

--- src/CMD_LIST.py
+++ src/CMD_LIST.py
@@ -3,10 +3,11 @@ CMD_LIST_DEFINE = {
     "exit|close|finish|goHome" : "exits the shell, put any number after for the exit code",
     "mkdir|make_directory|mkpath" : "makes a directory of the name you enter in",
     "touch|make_file|filecreate|FCRET" : "makes a file of the name you enter in, dont forget the file extension",
-    "help|pleaseHelp|stuck|h|MCSH_HELP" : "brings you to this screen"
+    "help|pleaseHelp|stuck|h|MCSH_HELP" : "brings you to this screen",
+    "ls|list|dir" : "List the Dir"
 }
 
-CMD_LIST_DEFINE_NONDICT = "echo|say|repeat|print : echo's what you put in\nexit|close|finish|goHome : exits the shell, put any number after for the exit code\nmkdir|make_directory|mkpath : makes a directory of the name you enter in\ntouch|make_file|filecreate|FCRET : makes a file of the name you enter in, dont forget the file extension\nhelp|pleaseHelp|stuck|h|MCSH_HELP : brings you to this screen"
+CMD_LIST_DEFINE_NONDICT = "if you enter a command thats not in this library then it will run as a bash command\n\necho|say|repeat|print : echo's what you put in\nexit|close|finish|goHome : exits the shell, put any number after for the exit code\nmkdir|make_directory|mkpath : makes a directory of the name you enter in\ntouch|make_file|filecreate|FCRET : makes a file of the name you enter in, dont forget the file extension\nhelp|pleaseHelp|stuck|h|MCSH_HELP : brings you to this screen\nls|dir|list : brings up whats in that directory\ncd|ChangeDir : Changes the Directory that MCSH is in\n"
 
 echo = {
     "echo",
@@ -53,4 +54,9 @@ ls = {
 cd = {
     "cd",
     "ChangeDir"
+}
+
+clear = {
+    "cls",
+    "clear"
 }
--- src/MCSH.py
+++ src/MCSH.py
@@ -4,20 +4,29 @@ import CMD_LIST
 from rich.console import Console
 
 console = Console()
+ranGood = False
 
+bashCmdRanGood = 0
 
-
-MCSH_VER = "0.1.4"
+MCSH_VER = "0.1.7"
 
 def main():
+    global bashCmdRanGood
     while True:
+        ranGood = False
+        
         print("\n")
-        os.system("pwd")
-        command = str(console.input("[red b i reverse] MCSH [/] >"))
+        if bashCmdRanGood != 0:
+            command = str(console.input("[red b i reverse] "+os.getcwd()+" [/] >"))
+        else:
+            command = str(console.input("[green b i reverse] "+os.getcwd()+" [/] >"))
+
+        bashCmdRanGood = 0
 
         for terms in CMD_LIST.echo:
             if command.split(" ")[0] == terms:
                 print(command.split(" ")[1])
+                ranGood = True
                 pass
             pass
         pass
@@ -26,8 +35,10 @@ def main():
             if command.split(" ")[0] == terms:
                 try:
                     sys.exit(command.split(" ")[1])
+                    anGood = True
                 except IndexError:
                     sys.exit()
+                    ranGood = True
                 except Exception as e:
                     print("an unknown error as occurs\n\n\n",e)
                 pass
@@ -39,6 +50,7 @@ def main():
                 dirname = command.split(" ")[1]
                 dirname = "mkdir " + dirname
                 os.system(dirname)
+                ranGood = True
                 pass
             pass
         pass
@@ -48,27 +60,50 @@ def main():
                 filename = command.split(" ")[1]
                 filename = "touch " + filename
                 os.system(filename)
+                ranGood = True
                 pass
             pass
 
         for terms in CMD_LIST.help:
             if command.split(" ")[0] == terms:
                 print(CMD_LIST.CMD_LIST_DEFINE_NONDICT)
+                ranGood = True
                 pass
             pass
         
         for terms in CMD_LIST.ls:
             if command.split(" ")[0] == terms:
                 os.system("ls")
+                ranGood = True
         
         for terms in CMD_LIST.cd:
             if command.split(" ")[0] == terms:
                 try:
                     print(command.split(" ")[1])
                     os.chdir(command.split(" ")[1])
+                    ranGood = True
                 except:
                     print("Current Directory:")
                     os.system("pwd")
+                    ranGood = True
+        
+        for terms in CMD_LIST.clear:
+            if command.split(" ")[0] == terms:
+                os.system("clear")
+                ranGood = True
+        
+        if command.split(" ")[0] == "b":
+            os.system(command.split(" ")[1])
+            ranGood = True
+        
+        if ranGood == False:
+            bashCmdRanGood = os.system(command)
+            ranGood = True
+        
+        if bashCmdRanGood != 0:
+            console.print("[blink bold white on red i]Invalid Command[/]: " + command, style="bold white on blue")
+
+        
 
 
 
@@ -76,5 +111,5 @@ def main():
     pass
 
 if __name__ == '__main__':
-    console.print("MessyCode Shell, built on top of BASH (C) 2022 9/9/22\nVersion: ",MCSH_VER, style="bold white on blue", justify="center")
+    console.print("MessyCode Shell (MCSH), built on top of BASH\nVersion: ",MCSH_VER, style="bold white on blue", justify="center")
     main()
